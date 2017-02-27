b58f55373176d2b943f7ba119a30a65ed1942d30
Davide Pugliese <dvd.pugliese@gmail.com>
2017-02-27 02:13:49 +0200

    work in progress

--- src/connection.js
+++ src/connection.js
@@ -1,53 +1,34 @@
+"use strict";
+exports.__esModule = true;
 /**
  * Created by spotted on 26/02/17.
  */
-import * as Rx from 'rxjs/Rx';
-
-import {Scraper} from "./scraper";
-
-
-
-export class Connection {
-
-     connection: any;
-
-    constructor(req:any, res:any) {
-        let url = req.param('url');
-
-        let scrapedData = Scraper.scrape(url);
-
-        let source = Rx.Observable.from(scrapedData);
-
-        let published = source.publish();
-
+var Rx = require("rxjs/Rx");
+var scraper_1 = require("./scraper");
+var Connection = (function () {
+    function Connection(req, res) {
+        var url = req.param('url');
+        var scrapedData = scraper_1.Scraper.scrape(url);
+        var source = Rx.Observable.from(scrapedData);
+        var published = source.publish();
         //useful for polymorphic kind of stuff: add a 'SourceA' argument
-
         // published.subscribe(createObserver('SourceA'));
-
         published.subscribe(this.createObserver(res));
-
         //disposable object that I can use later own, more for my own convenience, to remember
         //that this is the connection. Otherwise I could just simply use published.unsubscribe();
-
         // var connection = published.connect();
-
-        this.connection =  published.connect();
-
+        this.connection = published.connect();
         //if you need polymorphism place a tag parameter that basically receives SourceA
         // or whatever tag you decide
-
         // function createObserver(tag){
-
     }
-
-    createObserver(res: any) {
-
+    Connection.prototype.createObserver = function (res) {
         return {
-            next: function (response:any ) { res.json(response); },
-            error: function (err: any) { console.log('Error: %s', err); },
+            next: function (response) { res.json(response); },
+            error: function (err) { console.log('Error: %s', err); },
             complete: function () { console.log('Completed'); }
         };
-
-    }
-
-}
+    };
+    return Connection;
+}());
+exports.Connection = Connection;
