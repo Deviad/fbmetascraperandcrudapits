4d19356a410375c4cb619ec8b1138d5610fd99c1
Davide Pugliese <dvd.pugliese@gmail.com>
2017-03-05 19:06:54 +0200

    removed temporarily unneded imports

--- src/dataservice.ts
+++ src/dataservice.ts
@@ -2,16 +2,9 @@
  * Created by spotted on 26/02/17.
  */
 import { Observable } from "rxjs/Observable";
-// import { Observer } from 'rxjs/Observer';
-// import { Subscription } from 'rxjs/Subscription';
-import { Subscriber } from 'rxjs/Subscriber';
 import "rxjs/add/observable/from";
-import "rxjs/add/operator/publish";
-import "rxjs/add/observable/defer";
 import { Scraper } from './scraper';
-import { PromiseObservable } from 'rxjs/observable/PromiseObservable';
-import * as express from 'express';
-import {ConnectableObservable, Observer} from "rxjs";
+
 
 class DataService {
 
